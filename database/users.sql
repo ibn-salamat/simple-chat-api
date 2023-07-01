cb4da9688eb1f7c4596df11eb1145ec06f2e7fa7
nurlykhan <nurlykhan.salamatuly@samgau.com>
2023-07-01 17:13:51 +0600

    users_confirmation table query

--- database/users.sql
+++ database/users.sql
@@ -2,3 +2,10 @@ CREATE TABLE users (
     id SERIAL UNIQUE,
     email character varying(30) NOT NULL UNIQUE
 );
+
+CREATE TABLE users_confirmation (
+    email character varying(30) NOT NULL UNIQUE,
+    left_tries_count numeric DEFAULT 5 CHECK (left_tries_count <= 5 AND left_tries_count >= 0),
+    confirmed boolean DEFAULT false,
+    confirmation_code character(6)
+);
