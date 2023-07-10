ecfc20a3624a750cfdd01238f12c5c182f8a0fa3
nurlykhan <nurlykhan.salamatuly@samgau.com>
2023-07-11 04:57:31 +0600

    feat: check password

--- handlers/auth/sign-in.go
+++ handlers/auth/sign-in.go
@@ -1,23 +1,83 @@
 package auth
 
 import (
+	"database/sql"
 	"encoding/json"
-	"log"
+	"fmt"
+	"golang.org/x/crypto/bcrypt"
+	"ibn-salamat/simple-chat-api/database"
 	"net/http"
+	"strings"
 )
 
 func SignInHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 
-	body := response{
-		"message": "signin",
+	type Data struct {
+		Email    string `json:"email"`
+		Password string `json:"password"`
 	}
 
-	jsonBody, err := json.Marshal(body)
+	var data Data
+
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&data)
+
+	data.Email = strings.Trim(data.Email, " ")
+	data.Password = strings.Trim(data.Password, " ")
+
+	if err != nil || data.Email == "" || data.Password == "" {
+		jsonBody, _ := json.Marshal(response{
+			"errorMessage": "Incorrect json. Required fields: email, password.",
+			})
+
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(jsonBody)
+
+		return
+	}
+
+	var passwordHash string
+
+	err = database.DB.QueryRow(`
+	SELECT password
+	FROM
+	users
+	WHERE
+	email = $1
+	`, data.Email).Scan(&passwordHash)
 
 	if err != nil {
-		log.Fatalf("Error happened in JSON marshal. Err: %s", err)
+		errorMessage := err.Error()
+
+		if err == sql.ErrNoRows {
+			errorMessage = "User with this email is not found."
+		}
+
+		jsonBody, _ := json.Marshal(response{
+			"errorMessage": errorMessage,
+			})
+
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(jsonBody)
+
+		return
 	}
 
-	w.Write(jsonBody)
+	err = bcrypt.CompareHashAndPassword([]byte(passwordHash), []byte(data.Password))
+
+	if err != nil {
+		jsonBody, _ := json.Marshal(response{
+			"errorMessage": "Incorrect password",
+			})
+
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write(jsonBody)
+
+		return
+	}
+
+
+	fmt.Println("success")
+
 }
