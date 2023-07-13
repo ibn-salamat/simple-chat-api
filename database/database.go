da6b1d6810a0934d242ca7c3bc5360fa17490a6e
nurlykhan <nurlykhan.salamatuly@samgau.com>
2023-07-13 16:44:47 +0600

    refactor: use env values

--- database/database.go
+++ database/database.go
@@ -3,14 +3,23 @@ package database
 import (
 	"database/sql"
 	"fmt"
+	"ibn-salamat/simple-chat-api/helpers"
 	"log"
 )
 
 var DB *sql.DB
 
-var DBCredentials = fmt.Sprintf("host=%s port=%d user=%s "+
+var (
+	host     = helpers.GetEnvValue("PGHOST")
+	port     = helpers.GetEnvValue("PGPORT")
+	user     = helpers.GetEnvValue("PGUSER")
+	password = helpers.GetEnvValue("PGPASSWORD")
+	dbname   = helpers.GetEnvValue("PGDATABASE")
+)
+
+var DBCredentials = fmt.Sprintf("host=%s port=%s user=%s "+
 	"password=%s dbname=%s sslmode=disable",
-	"localhost", 5432, "admin", "postgres", "simple-chat")
+	host, port, user, password, dbname)
 
 func OpenDB() {
 	db, err := sql.Open("postgres", DBCredentials)
