810e3d39169d3f8d7e8fd217d2bbd3d3f973b6db
nurlykhan <nurlykhan.salamatuly@samgau.com>
2023-07-13 17:03:33 +0600

    fix: use filepath

--- helpers/config.go
+++ helpers/config.go
@@ -3,12 +3,13 @@ package helpers
 import (
 	"log"
 	"os"
+	"path/filepath"
 
 	"github.com/joho/godotenv"
 )
 
 func GetEnvValue(key string) string {
-	err := godotenv.Load(".env")
+	err := godotenv.Load(filepath.Join("./", ".env"))
 
 	if err != nil {
 		log.Fatalf("Error loading .env file")
