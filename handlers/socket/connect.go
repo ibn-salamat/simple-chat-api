c60475baf5e2441a635bed03d3092f0d8c738d2e
nurlykhan <nurlykhan.salamatuly@samgau.com>
2023-07-13 06:04:22 +0600

    feat: add test message every 5 second

--- handlers/socket/connect.go
+++ handlers/socket/connect.go
@@ -1,19 +1,45 @@
 package socket
 
 import (
+	"fmt"
 	"golang.org/x/net/websocket"
 	"log"
+	"math/rand"
+	"time"
 )
 
 type response map[string]string
+
 func SocketHandler(ws *websocket.Conn) {
 	authorizationError := CheckAuthorization(ws)
 
 	if authorizationError != nil {
 		return
+	} else {
+		log.Println("User connected")
 	}
 
-	log.Println("User connected")
+	defer HandleReceive(ws)
 
-	HandleReceive(ws)
+	// test send message
+	ticker := time.NewTicker(time.Second * 1)
+
+	go func() {
+		for {
+			select {
+			case <- ticker.C:
+				// TODO
+				authorizationError = CheckAuthorization(ws)
+
+				if authorizationError != nil {
+					ticker.Stop()
+					return
+				}
+
+				if err := websocket.Message.Send(ws, fmt.Sprintf("Message from server: %i", rand.Int())); err != nil {
+					log.Println(err)
+				}
+			}
+		}
+	}()
 }
