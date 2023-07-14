9cd8c8bf10efe9358b24921dda290f9e1d9e466b
nurlykhan <nurlykhan.salamatuly@samgau.com>
2023-07-14 11:39:09 +0600

    fix: error functional

--- handlers/socket/check-authorization.go
+++ handlers/socket/check-authorization.go
@@ -3,31 +3,50 @@ package socket
 import (
 	"encoding/json"
 	"errors"
-	"golang.org/x/net/websocket"
 	"ibn-salamat/simple-chat-api/tools"
 	"ibn-salamat/simple-chat-api/types"
 	"log"
 	"net/http"
+
+	"golang.org/x/net/websocket"
 )
 
 func CheckAuthorization(ws *websocket.Conn) error {
 	token, resultErr := ws.Request().Cookie("token")
-	
+
 	// check token exists
 	if resultErr != nil {
 		if resultErr == http.ErrNoCookie {
 			resultErr = errors.New("token is not exist in cookies")
 		}
+
+		jsonBody, _ := json.Marshal(types.ResponseMap{
+			"errorMessage": resultErr.Error(),
+		})
+
+		_, err := ws.Write(jsonBody)
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		err = ws.Close()
+		if err != nil {
+			log.Println(err)
+		}
+
+		return resultErr
 	}
-	
+
 	resultErr = tools.CheckToken(token.Value)
-	
+
 	if resultErr != nil {
 		jsonBody, _ := json.Marshal(types.ResponseMap{
 			"errorMessage": resultErr.Error(),
 		})
 
 		_, err := ws.Write(jsonBody)
+
 		if err != nil {
 			log.Println(err)
 		}
@@ -37,6 +56,6 @@ func CheckAuthorization(ws *websocket.Conn) error {
 			log.Println(err)
 		}
 	}
-	
+
 	return resultErr
 }
