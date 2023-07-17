cb0ae4892909d6c7a67e1b385d90b978e5472d57
nurlykhan <nurlykhan.salamatuly@samgau.com>
2023-07-17 21:01:30 +0600

    refactor: delete extra headers

--- middlewares/headers.go
+++ middlewares/headers.go
@@ -6,8 +6,7 @@ func SetBasicHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
-		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
-		w.Header().Set("Access-Control-Allow-Headers", "X-Requested-With,content-type")
+		w.Header().Set("Access-Control-Allow-Methods", "*")
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
 
 		next.ServeHTTP(w, r)
