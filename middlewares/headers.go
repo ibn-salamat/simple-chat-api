bdf3ddf7b086c074f914af320a4e81d68421fd48
nurlykhan <nurlykhan.salamatuly@samgau.com>
2023-07-17 20:00:25 +0600

    fix: add new header

--- middlewares/headers.go
+++ middlewares/headers.go
@@ -6,6 +6,7 @@ func SetBasicHeaders(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, PATCH, DELETE")
 
 		next.ServeHTTP(w, r)
 	})
