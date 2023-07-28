3899ca17c6b1bf9fa5aa6922455d64087f01ff7b
nurlykhan <nurlykhan.salamatuly@samgau.com>
2023-07-29 00:11:49 +0600

    refactor: split chat sql

--- database/users.sql
+++ database/users.sql
@@ -10,11 +10,3 @@ CREATE TABLE users_confirmation (
     confirmed boolean DEFAULT false,
     confirmation_code character(6)
 );
-
-CREATE TABLE general_chat_messages (
-    id UUID NOT NULL,
-    email character varying(30) NOT NULL,
-    message_type character varying(30) NOT NULL,
-    message_content text,
-  	created_at timestamp
-);
