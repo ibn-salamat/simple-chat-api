0a2af843516ba1af6457559aed0ec009334cb975
nurlykhan <nurlykhan.salamatuly@samgau.com>
2023-07-29 00:09:15 +0600

    feat: add message to data

--- handlers/socket/sendToClients.go
+++ handlers/socket/sendToClients.go
@@ -2,7 +2,9 @@ package socket
 
 import (
 	"encoding/json"
+	"ibn-salamat/simple-chat-api/database"
 	"ibn-salamat/simple-chat-api/types"
+	"log"
 	"time"
 
 	"github.com/google/uuid"
@@ -10,19 +12,24 @@ import (
 )
 
 func sendToClients(email string, messageType string, content string) {
+	id := uuid.New()
+	date := time.Now().UTC().Format(time.RFC3339)
 	jsonBody, _ := json.Marshal(types.ResponseMap{
-		"id":      uuid.New(),
+		"id":      id,
 		"type":    messageType,
 		"email":   email,
 		"content": content,
-		"date":    time.Now().UTC().Format(time.RFC3339),
+		"date":    date,
 	})
 
-	// id
-	// email
-	// message_type
-	// message_content
-	// created_at
+	_, err := database.DB.Exec(`
+		INSERT INTO general_chat_messages (id, email, message_type, message_content, created_at)
+		VALUES ($1, $2, $3, $4, $5)
+	`, id, email, messageType, content, date)
+
+	if err != nil {
+		log.Println(err)
+	}
 
 	for conn := range clients {
 		conn.WriteMessage(websocket.TextMessage, jsonBody)
