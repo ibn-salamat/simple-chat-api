cce63b1168a5f3e6c2e807f7cedf6f4379cd974c
nurlykhan <nurlykhan.salamatuly@samgau.com>
2023-07-28 23:14:02 +0600

    feat: add uid

--- handlers/socket/sendToClients.go
+++ handlers/socket/sendToClients.go
@@ -5,15 +5,17 @@ import (
 	"ibn-salamat/simple-chat-api/types"
 	"time"
 
+	"github.com/google/uuid"
 	"github.com/gorilla/websocket"
 )
 
 func sendToClients(email string, messageType string, content string) {
 	jsonBody, _ := json.Marshal(types.ResponseMap{
+		"id":      uuid.New(),
 		"type":    messageType,
 		"email":   email,
 		"content": content,
-		"date":    time.Now().Format(time.RFC3339),
+		"date":    time.Now().UTC().Format(time.RFC3339),
 	})
 
 	for conn := range clients {
